7b48c2f1861f941efd765d37299ad16f1e49d2e3
Katya Kulikova <katya.kulikova@jetbrains.com>
2017-01-31 14:03:53 +0300

    Initial commit

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharedProjectTest ;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        Class2 class2 = new Class2();
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System ;
+using System.Collections.Generic ;
+using System.Linq ;
+using System.Text ;
+using System.Threading.Tasks ;
+using SharedProjectTest ;
+
+namespace ConsoleApplication1{
+    class Program{
+        static void Main (string[] args)
+        {
+            Class2 class2 = new Class2() ;
+        }
+    }
+}
